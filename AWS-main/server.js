8fab50b88fe2c30932fd269d3fdb2d146363450f
oslo990 <bridgeoslo9@gmail.com>
2025-03-22 14:56:19 +0100

    nchalahya rebiiiiiipppyyyzzzzxxxxxxeeeeeaaaaaaaaaaaatttttttttttooooooooooooo

--- AWS-main/server.js
+++ AWS-main/server.js
@@ -108,7 +108,7 @@ const redirectIfAuthenticated = (req, res, next) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://okay-fawn.vercel.app/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     console.log("✅ Google OAuth Callback reçu :", profile);
 
